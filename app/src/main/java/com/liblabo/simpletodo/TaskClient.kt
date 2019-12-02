f44ecf1af83c65f429110332f64517b40cc6254e
Hiromitsu Sasaki <hiromitsu.sasaki@gmail.com>
2019-12-03 08:39:12 +0900

    [WIP] implement get task list from server

--- app/src/main/java/com/liblabo/simpletodo/TaskClient.kt
+++ app/src/main/java/com/liblabo/simpletodo/TaskClient.kt
@@ -1,19 +1,35 @@
 package com.liblabo.simpletodo
 
-import okhttp3.OkHttpClient
+import okhttp3.*
+import java.io.IOException
 
 class TaskClient {
+    companion object {
+        const val LIST_URL: String = ""
+        const val CREATE_URL: String = ""
+        const val UPDATE_URL: String = ""
+        const val DELETE_URL: String = ""
+    }
     val client: OkHttpClient = OkHttpClient()
+    val JSON: MediaType = MediaType.get("application/json; charset=utf-8")
 
-    fun list() : Array<Task>? {
-        // get task list
-        return null;
+    fun list() {
+        val request = Request.Builder()
+            .url(LIST_URL).get().build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
     }
 
     fun create(task: Task) {
-        // create new task
+        // create new task }
     }
-
     fun update(task: Task) {
         // update task
     }
