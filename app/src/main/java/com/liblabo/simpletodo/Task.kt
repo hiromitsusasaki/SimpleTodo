b3b954ea69a5efd8eff1989a4f7800db438404f0
Hiromitsu Sasaki <hiromitsu.sasaki@gmail.com>
2019-12-03 08:23:29 +0900

    add 'id' to Task data model

--- app/src/main/java/com/liblabo/simpletodo/Task.kt
+++ app/src/main/java/com/liblabo/simpletodo/Task.kt
@@ -2,4 +2,4 @@ package com.liblabo.simpletodo
 
 import java.util.*
 
-data class Task (val title: String, val description: String, val created_at: Date, val updated_at: Date)
+data class Task (val id: Int, val title: String, val description: String, val created_at: Date, val updated_at: Date)
