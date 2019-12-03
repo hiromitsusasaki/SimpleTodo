142491a8c109daad7b5d0740caf575e86a97bda6
Hiromitsu Sasaki <hiromitsu.sasaki@gmail.com>
2019-12-03 22:27:16 +0900

    add inplementation of Callback interface

--- app/src/main/java/com/liblabo/simpletodo/DetailActivity.kt
+++ app/src/main/java/com/liblabo/simpletodo/DetailActivity.kt
@@ -2,11 +2,23 @@ package com.liblabo.simpletodo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Response
+import java.io.IOException
 
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity(), Callback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onFailure(call: Call, e: IOException) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onResponse(call: Call, response: Response) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
--- app/src/main/java/com/liblabo/simpletodo/MainActivity.kt
+++ app/src/main/java/com/liblabo/simpletodo/MainActivity.kt
@@ -2,11 +2,23 @@ package com.liblabo.simpletodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Response
+import java.io.IOException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , Callback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onFailure(call: Call, e: IOException) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onResponse(call: Call, response: Response) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
