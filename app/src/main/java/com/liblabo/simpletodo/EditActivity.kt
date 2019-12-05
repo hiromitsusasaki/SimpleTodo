52dadfa7a8e73bd3f35ee0728191aba4bcb80ced
Hiromitsu Sasaki <hiromitsu.sasaki@gmail.com>
2019-12-05 23:10:01 +0900

    add edit screen

--- app/src/main/java/com/liblabo/simpletodo/EditActivity.kt
+++ app/src/main/java/com/liblabo/simpletodo/EditActivity.kt
@@ -1,17 +1,31 @@
 package com.liblabo.simpletodo
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.Response
 import java.io.IOException
 
-class DetailActivity : AppCompatActivity(), Callback {
-
+class EditActivity : AppCompatActivity(), Callback, View.OnClickListener {
+    val gson: Gson = Gson()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_edit)
+        val editTitle = findViewById<EditText>(R.id.edit_txt_title)
+        val editDescription = findViewById<EditText>(R.id.edit_txt_description)
+        val btnSave = findViewById<Button>(R.id.btn_save)
+        if (intent.hasExtra("TASK")) {
+            val task = gson.fromJson(intent.getStringExtra("TASK"), Task::class.java)
+            editTitle.setText(task.title)
+            editDescription.setText(task.description)
+            btnSave.setText("Update")
+            btnSave.setOnClickListener(this)
+        }
     }
 
     override fun onFailure(call: Call, e: IOException) {
@@ -20,5 +34,9 @@ class DetailActivity : AppCompatActivity(), Callback {
 
     override fun onResponse(call: Call, response: Response) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onClick(v: View?) {
+
     }
 }
